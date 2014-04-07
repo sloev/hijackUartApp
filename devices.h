f9896a1eb5276fdc165ed17140499d413af99636
sloev <johannesgj@gmail.com>
2014-04-07 09:14:47 +0200

    lol

---
+++ devices.h
@@ -0,0 +1,13 @@
+//
+//  devices.h
+//  Smokalyzer
+//
+//  Created by johannes on 4/6/14.
+//  Copyright (c) 2014 Andrew Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface devices : UITableViewController
+
+@end
---
+++ samples.h
@@ -0,0 +1,13 @@
+//
+//  samples.h
+//  Smokalyzer
+//
+//  Created by johannes on 4/6/14.
+//  Copyright (c) 2014 Andrew Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface samples : UITableViewController
+
+@end
